2d1394e60aa6e238968df5c437bd5461216aede6
printfn <printfn@users.noreply.github.com>
2016-02-06 09:07:19 +1300

    Much better names for control characters

--- data.js
+++ data.js
@@ -6,46 +6,73 @@ function getCodepointDescription(codepoint, name) {
 	return name + ' ' + ctos([codepoint]);
 }
 
-function mergeNewAndLegacyNames(data_file_name, data_file_legacy_name) {
-	if (data_file_legacy_name && data_file_legacy_name != '')
-		return data_file_name + ' (' + data_file_legacy_name + ')';
-	return data_file_name;
-}
-
 function getRangeFunctionForName(name) {
 	return function(codepoint) {
 		return getCodepointDescription(codepoint, name);
 	}
 }
 
-function initUnicodeData(completion) {
+function initAliasData(completion) {
 	var client = new XMLHttpRequest();
-	client.open('GET', 'UnicodeData.txt');
+	client.open('GET', 'NameAliases.txt');
 	client.onreadystatechange = function() { 
 		if (client.readyState == 4 && client.status == 200) {
 			var dataStrings = client.responseText.split('\n');
-			window.data = [];
-			for (var i in dataStrings) {
-				var data_line = dataStrings[i].split(';');
-				if (data_line[1].endsWith(', First>')) {
-					window.ranges.push([
-						parseInt('0x' + data_line[0]),
-						parseInt('0x' + dataStrings[parseInt(i)+1].split(';')[0]),
-						getRangeFunctionForName(data_line[1].substring(1, data_line[1].length - 8))
-					]);
-				} else if (data_line[1].endsWith(', Last>')) {
-					continue;
-				} else {
-					window.data[parseInt('0x' + data_line[0])] = getCodepointDescription(
-						'0x' + data_line[0],
-						mergeNewAndLegacyNames(data_line[1], data_line[10])
-					);
-				}
+			window.aliases = [];
+			window.controlAliases = [];
+			for (var i = 0; i < dataStrings.length; ++i) {
+				var splitLine = dataStrings[i].split(';');
+				var codepoint = parseInt('0x' + splitLine[0]);
+				var alias = splitLine[1];
+				window.aliases.push({codepoint: codepoint, alias: alias});
+				if (splitLine[2] == 'control')
+					window.controlAliases.push({codepoint: codepoint, alias: alias});
 			}
 			completion();
 		}
 	}
 	client.send();
+}
+
+function initUnicodeData(completion) {
+	initAliasData(function() {
+		var client = new XMLHttpRequest();
+		client.open('GET', 'UnicodeData.txt');
+		client.onreadystatechange = function() { 
+			if (client.readyState == 4 && client.status == 200) {
+				var dataStrings = client.responseText.split('\n');
+				window.data = [];
+				for (var i in dataStrings) {
+					var data_line = dataStrings[i].split(';');
+					if (data_line[1].endsWith(', First>')) {
+						window.ranges.push([
+							parseInt('0x' + data_line[0]),
+							parseInt('0x' + dataStrings[parseInt(i)+1].split(';')[0]),
+							getRangeFunctionForName(data_line[1].substring(1, data_line[1].length - 8))
+						]);
+					} else if (data_line[1].endsWith(', Last>')) {
+						continue;
+					} else if (data_line[1] == '<control>') {
+						var name = [];
+						var codepoint = parseInt('0x' + data_line[0]);
+						for (var j = 0; j < window.controlAliases.length; ++j) {
+							if (window.controlAliases[j].codepoint == codepoint) {
+								name.push(window.controlAliases[j].alias);
+							}
+						}
+						window.data[parseInt('0x' + data_line[0])] = getCodepointDescription(
+							'0x' + data_line[0],
+							'<control> (' + name.join(' / ') + ')'
+						);
+					} else {
+						window.data[parseInt('0x' + data_line[0])] = getCodepointDescription('0x' + data_line[0], data_line[1]);
+					}
+				}
+				completion();
+			}
+		}
+		client.send();
+	});
 }
 
 function getUnicodeData(codepoint) {
