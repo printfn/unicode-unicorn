1eecfcd01efb944da0fc99d83b95d9fde5ac1203
printfn <printfn@users.noreply.github.com>
2019-03-16 01:13:48 +0100

    Clean up search.ts

--- docs/build/main.js
+++ docs/build/main.js
@@ -763,21 +763,31 @@ $(document).ready(function () {
     });
 });
 let global_search_strings = [];
-function getSearchString(codepoint) {
-    let res = `${ctos([codepoint])}|U+${itos(codepoint, 16, 4)}|cp:${codepoint}|name:${getName(codepoint, true)}|block:${getBlockForCodepoint(codepoint)}|script:${getScriptForCodepoint(codepoint).replace(/_/g, ` `)}|category:${getCharacterCategoryName(codepoint)}`;
+function getTermsForSearchString(array, codepoint, prefix) {
+    let entry = array[codepoint];
+    if (!entry) {
+        return '';
+    }
+    return `${prefix}${entry.split(', ').join(prefix)}`;
+}
+function getAliasSearchStringForCodepoint(codepoint) {
+    let res = '';
     for (let i = 0; i < global_aliases.length; ++i) {
         if (global_aliases[i].codepoint == codepoint) {
             res += `|name:${global_aliases[i].alias}`;
         }
     }
-    if (global_han_meanings[codepoint])
+    return res;
+}
+function getSearchString(codepoint) {
+    let res = `${ctos([codepoint])}|U+${itos(codepoint, 16, 4)}|cp:${codepoint}|name:${getName(codepoint, true)}|block:${getBlockForCodepoint(codepoint)}|script:${getScriptForCodepoint(codepoint).replace(/_/g, ` `)}|category:${getCharacterCategoryName(codepoint)}`;
+    res += getAliasSearchStringForCodepoint(codepoint);
+    if (global_han_meanings[codepoint]) {
         res += global_han_meanings[codepoint];
-    if (global_kun_readings[codepoint])
-        res += `|kun:${global_kun_readings[codepoint].split(`, `).join(`|kun:`)}`;
-    if (global_on_readings[codepoint])
-        res += `|on:${global_on_readings[codepoint].split(`, `).join(`|on:`)}`;
-    if (global_mandarin_readings[codepoint])
-        res += `|mandarin:${global_mandarin_readings[codepoint].split(`, `).join(`|mandarin:`)}`;
+    }
+    res += getTermsForSearchString(global_kun_readings, codepoint, '|kun:');
+    res += getTermsForSearchString(global_on_readings, codepoint, '|on:');
+    res += getTermsForSearchString(global_mandarin_readings, codepoint, '|mandarin:');
     return res.toUpperCase();
 }
 function initializeSearchStrings() {
@@ -798,24 +808,30 @@ function testSearch(searchString, words) {
     }
     return true;
 }
-function searchCodepoints(str) {
-    const results = [];
+function wordsFromSearchExpression(str) {
     str = str.toUpperCase();
     const words = str.split(`,`);
     for (let i = 0; i < words.length; ++i) {
         words[i] = words[i].trim();
     }
+    return words;
+}
+function searchCodepoints(str) {
+    const results = [];
+    const words = wordsFromSearchExpression(str);
     for (let i = 0; i < global_all_assigned_ranges.length; ++i) {
         const range = global_all_assigned_ranges[i];
         const end = range.endCodepoint;
         for (let c = range.startCodepoint; c <= end; ++c) {
             const searchString = global_search_strings[c];
-            if (!searchString)
+            if (!searchString) {
                 continue;
+            }
             if (testSearch(searchString, words)) {
                 results.push(c);
-                if (results.length >= 256)
+                if (results.length >= 256) {
                     return results;
+                }
             }
         }
     }
--- docs/ts/search.ts
+++ docs/ts/search.ts
@@ -1,5 +1,26 @@
 let global_search_strings: { [codepoint: number]: string; } = [];
 
+function getTermsForSearchString(array: { [codepoint: number]: string; }, codepoint: number, prefix: string) {
+	let entry = array[codepoint];
+	if (!entry) {
+		return '';
+	}
+
+	return `${prefix}${entry.split(', ').join(prefix)}`;
+}
+
+function getAliasSearchStringForCodepoint(codepoint: number) {
+	let res = '';
+
+	for (let i = 0; i < global_aliases.length; ++i) {
+		if (global_aliases[i].codepoint == codepoint) {
+			res += `|name:${global_aliases[i].alias}`;
+		}
+	}
+
+	return res;
+}
+
 function getSearchString(codepoint: number) {
 	let res = `${ctos([codepoint])
 	}|U+${itos(codepoint, 16, 4)
@@ -8,19 +29,17 @@ function getSearchString(codepoint: number) {
 	}|block:${getBlockForCodepoint(codepoint)
 	}|script:${getScriptForCodepoint(codepoint).replace(/_/g, ` `)
 	}|category:${getCharacterCategoryName(codepoint)}`;
-	for (let i = 0; i < global_aliases.length; ++i) {
-		if (global_aliases[i].codepoint == codepoint) {
-			res += `|name:${global_aliases[i].alias}`;
-		}
-	}
-	if (global_han_meanings[codepoint])
+
+	res += getAliasSearchStringForCodepoint(codepoint);
+
+	if (global_han_meanings[codepoint]) {
 		res += global_han_meanings[codepoint];
-	if (global_kun_readings[codepoint])
-		res += `|kun:${global_kun_readings[codepoint].split(`, `).join(`|kun:`)}`;
-	if (global_on_readings[codepoint])
-		res += `|on:${global_on_readings[codepoint].split(`, `).join(`|on:`)}`;
-	if (global_mandarin_readings[codepoint])
-		res += `|mandarin:${global_mandarin_readings[codepoint].split(`, `).join(`|mandarin:`)}`;
+	}
+
+	res += getTermsForSearchString(global_kun_readings, codepoint, '|kun:');
+	res += getTermsForSearchString(global_on_readings, codepoint, '|on:');
+	res += getTermsForSearchString(global_mandarin_readings, codepoint, '|mandarin:');
+
 	return res.toUpperCase();
 }
 
@@ -44,26 +63,34 @@ function testSearch(searchString: string, words: string[]) {
 	return true;
 }
 
-function searchCodepoints(str: string) {
-	const results: number[] = [];
-
+function wordsFromSearchExpression(str: string) {
 	str = str.toUpperCase();
 	const words = str.split(`,`);
 	for (let i = 0; i < words.length; ++i) {
 		words[i] = words[i].trim();
 	}
+	return words;
+}
+
+function searchCodepoints(str: string) {
+	const results: number[] = [];
+
+	const words = wordsFromSearchExpression(str);
 
 	for (let i = 0; i < global_all_assigned_ranges.length; ++i) {
 		const range = global_all_assigned_ranges[i];
 		const end = range.endCodepoint;
 		for (let c = range.startCodepoint; c <= end; ++c) {
 			const searchString = global_search_strings[c];
-			if (!searchString)
+			if (!searchString) {
 				continue;
+			}
+
 			if (testSearch(searchString, words)) {
 				results.push(c);
-				if (results.length >= 256)
+				if (results.length >= 256) {
 					return results;
+				}
 			}
 		}
 	}
