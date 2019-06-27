093261d29ee9a4c4b9abed0d8122d42c4b5bfe41
printfn <printfn@users.noreply.github.com>
2019-06-27 12:59:17 +0200

    Add initial CF script

---
+++ cloudflare/lsr/script.js
@@ -0,0 +1,15 @@
+// language subtag registry
+// upload script
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  return new Response('hello world', {status: 200})
+}
+
