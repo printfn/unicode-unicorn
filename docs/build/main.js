217029aaeb49a992346c89216428ab16a750665b
printfn <printfn@users.noreply.github.com>
2020-02-25 19:15:31 +1300

    Hopefully fix #1

--- docs/build/main.js
+++ docs/build/main.js
@@ -436,7 +436,7 @@ $(document).ready(function () {
                 else if (arg[0] == `str`) {
                     // search queries via the omnibox are URL-escaped, and spaces
                     // are converted to '+'.
-                    setStr(stoc(unescape(arg[1].replace(/\+/g, ' '))));
+                    setStr(utf8.decode(unescape(arg[1].replace(/\+/g, ' '))));
                 }
             }
         };
