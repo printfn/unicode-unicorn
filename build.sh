6bb9440efe5f3b68e6fb10bfb7d3561d339a9dd1
printfn <printfn@users.noreply.github.com>
2020-02-26 19:48:54 +1300

    Move WASM loader into libs js

--- build.sh
+++ build.sh
@@ -9,9 +9,17 @@ mkdir -p docs/build
 
 node compile-unicode-data.js
 
+# this depends on compile-unicode-data
 ./node_modules/.bin/tsc
 
+# also depends only on compile-unicode-data
+cargo fmt --manifest-path docs/unicode-rustwasm/Cargo.toml
+DATA_DIR="$(pwd)/data" wasm-pack build --target no-modules docs/unicode-rustwasm
+rm docs/unicode-rustwasm/pkg/.gitignore
+
+# depends on cargo/wasm build
 {
+	cat docs/unicode-rustwasm/pkg/unicode_rustwasm.js
 	cat node_modules/jquery/dist/jquery.min.js
 	cat node_modules/bootstrap/dist/js/bootstrap.min.js
 	cat node_modules/chosen-js/chosen.jquery.min.js
@@ -20,13 +28,11 @@ node compile-unicode-data.js
 	cat node_modules/punycode/punycode.js
 } > docs/build/libs.js
 
+# independent
 cp node_modules/bootstrap/dist/css/bootstrap.min.css docs/build/
 cp node_modules/bootstrap/dist/css/bootstrap.min.css.map docs/build/
 cp node_modules/chosen-js/chosen.min.css docs/build/
 
+# independent
 cp node_modules/chosen-js/chosen-sprite.png docs/build/
 cp node_modules/chosen-js/chosen-sprite@2x.png docs/build/
-
-cargo fmt --manifest-path docs/unicode-rustwasm/Cargo.toml
-DATA_DIR="$(pwd)/data" wasm-pack build --target no-modules docs/unicode-rustwasm
-rm docs/unicode-rustwasm/pkg/.gitignore
