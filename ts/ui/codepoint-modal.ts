7fe31904b781c68e5018d9d9861d536f60d0fde5
printfn <printfn@users.noreply.github.com>
2020-02-27 16:46:57 +1300

    Remove more jQuery

--- ts/ui/codepoint-modal.ts
+++ ts/ui/codepoint-modal.ts
@@ -1,37 +1,55 @@
+function getElementById(id: string) {
+  let element = document.getElementById(id);
+  if (!element) {
+    throw `Unable to find element #${id}`;
+  }
+  return element;
+}
+
+function selectedOption(selectId: string) {
+  let elem = getElementById(selectId) as HTMLSelectElement;
+  let options = elem.selectedOptions;
+  if (options.length != 1)
+    throw "The number of selected options is not 1";
+  return options[0];
+}
+
 function tryFillElement(id: string, value: string) {
+  let element = getElementById(id);
   if (value) {
-    $(`#${id}`).show();
-    $(`#${id}-content`).text(value);
+    element.style.display = '';
+    let contentElement = getElementById(`${id}-content`);
+    contentElement.textContent = value;
   } else {
-    $(`#${id}`).hide();
+    element.style.display = 'none';
   }
 }
 
 function showCodepageDetail(codepoint: number) {
-  $(`#detail-codepoint-hex`).text(itos(codepoint, 16, 4));
-  $(`#detail-codepoint-decimal`).text(codepoint);
-  $(`#detail-name`).html(`"${getName(codepoint)}"`);
-  $(`#detail-character`).html(displayCodepoint(codepoint));
-  $(`#detail-character-raw`).text(ctos([codepoint]));
-  $(`#detail-character-textbox`).val(ctos([codepoint]));
-  $(`#detail-category`).text(`${getCharacterCategoryCode(codepoint)} (${getCharacterCategoryName(codepoint)})`);
-  $(`#detail-block`).text(getBlockForCodepoint(codepoint).replace(/_/g, ` `));
-  $(`#detail-script`).text(getScriptForCodepoint(codepoint).replace(/_/g, ` `));
+  getElementById('detail-codepoint-hex').textContent = itos(codepoint, 16, 4);
+  getElementById('detail-codepoint-decimal').textContent = codepoint.toString();
+  getElementById('detail-name').innerHTML = `"${getName(codepoint)}"`;
+  getElementById('detail-character').innerHTML = displayCodepoint(codepoint);
+  getElementById('detail-character-raw').textContent = ctos([codepoint]);
+  (getElementById('detail-character-textbox') as HTMLInputElement).value = ctos([codepoint]);
+  getElementById('detail-category').textContent = `${getCharacterCategoryCode(codepoint)} (${getCharacterCategoryName(codepoint)})`;
+  getElementById('detail-block').textContent = getBlockForCodepoint(codepoint).replace(/_/g, ` `);
+  getElementById('detail-script').textContent = getScriptForCodepoint(codepoint).replace(/_/g, ` `);
   const matchingAliases: string[] = [];
   for (let i = 0; i < global_aliases.length; ++i) {
     if (global_aliases[i].codepoint == codepoint)
       matchingAliases.push(global_aliases[i].alias);
   }
-  tryFillElement('detail-aliases', matchingAliases.join(`, `));
+  tryFillElement('detail-aliases', matchingAliases.join(', '));
   tryFillElement('detail-meaning', global_han_meanings[codepoint]);
   tryFillElement('detail-mandarin', global_mandarin_readings[codepoint]);
   tryFillElement('detail-kun', global_kun_readings[codepoint]);
   tryFillElement('detail-on', global_on_readings[codepoint]);
   const variationSequences = variationSequencesForCodepoint(codepoint).concat(ideographicVariationSequencesForCodepoint(codepoint));
   if (variationSequences.length === 0) {
-    $(`#detail-variation-sequences`).hide();
+    getElementById('detail-variation-sequences').style.display = 'none';
   } else {
-    $(`#detail-variation-sequences`).show();
+    getElementById('detail-variation-sequences').style.display = '';
     let variationsString = ``;
     for (let i = 0; i < variationSequences.length; ++i) {
       let vs = variationSequences[i];
@@ -48,34 +66,42 @@ function showCodepageDetail(codepoint: number) {
         variationsString += `</i>`;
       }
     }
-    $(`#detail-variation-sequences-content`).html(variationsString);
+    getElementById('detail-variation-sequences-content').innerHTML = variationsString;
   }
 
   let encodingsString = ``;
-  $(`#outputEncoding option`).each(function(i, e) {
-    const encoding = $(e).text();
+  for (let elem of Array.from((getElementById('outputEncoding') as HTMLSelectElement).options)) {
+    let encoding = elem.textContent;
+    if (!encoding) {
+      throw "Unable to get encoding (via textContent) from element";
+    }
     const html = encodeOutput(
-      $(`#byteOrderMark option:selected`).text(),
+      selectedOption('byteOrderMark').textContent!,
       encoding,
-      $(`#outputFormat option:selected`).text(),
+      selectedOption('outputFormat').textContent!,
       [codepoint]
     );
-    if (html.startsWith(`<span`))
-      return;
+    if (html.startsWith(`<span`)) {
+      continue;
+    }
     encodingsString += `${encoding}: ${html}\n`;
-  });
+  };
 
-  $(`#detail-encoding-outputs`).html(encodingsString);
+  getElementById('detail-encoding-outputs').innerHTML = encodingsString;
 
-  $(`#detail-previous-cp`).attr(`data-cp`, itos(previousCodepoint(codepoint), 10));
-  $(`#detail-next-cp`).attr(`data-cp`, itos(nextCodepoint(codepoint), 10));
+  getElementById('detail-previous-cp').setAttribute('data-cp', itos(previousCodepoint(codepoint), 10));
+  getElementById('detail-next-cp').setAttribute('data-cp', itos(nextCodepoint(codepoint), 10));
   
   jQueryModal(`#codepoint-detail`, `show`);
 }
 
 // called from button in modal dialog to navigate to a different codepoint
 function changeDetail(elem: HTMLElement) {
-  $(elem).blur(); // remove focus
-  const codepointToShow = parseInt($(elem).attr(`data-cp`), 10);
+  elem.blur(); // remove focus
+  const attr = elem.getAttribute('data-cp');
+  if (!attr) {
+    throw "Unable to find 'data-cp' attribute to find codepoint to jump to";
+  }
+  const codepointToShow = parseInt(attr, 10);
   showCodepageDetail(codepointToShow);
 }
