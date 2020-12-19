e4bb50588214c2371f33f6489d4501acc2de4984
printfn <printfn@users.noreply.github.com>
2020-12-19 22:47:54 +1300

    Move Sentry to yarn module

---
+++ src/sentry.ts
@@ -0,0 +1,9 @@
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+    dsn: 'https://08cf17c769734a8a8a5b95cacde0c250@o491618.ingest.sentry.io/5557471',
+    integrations: [new (Sentry.Integrations as any).BrowserTracing()],
+
+    tracesSampleRate: 1.0,
+});
