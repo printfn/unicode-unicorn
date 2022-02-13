ed65ba72dbd95c8b30cf9bdb27df91edc89377c1
Rhys Davies <rhys@memes.nz>
2022-02-13 21:25:57 +1300

    Fix prettier formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,44 @@
 const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'production',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'source-map',
-  experiments: {
-    syncWebAssembly: true,
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: "html" },
-        { from: "favicon" },
-        { from: "data/compiled-data.json"}
-      ],
-    }),
-    new WasmPackPlugin({
-      crateDirectory: path.resolve(__dirname, 'wasm'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+    entry: './src/index.ts',
+    mode: 'production',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    experiments: {
+        syncWebAssembly: true,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'html' }, { from: 'favicon' }, { from: 'data/compiled-data.json' }],
+        }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, 'wasm'),
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
