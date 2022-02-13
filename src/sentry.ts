002975335119392407eeffa0ba341fa7f502b48c
Rhys Davies <rhys@memes.nz>
2022-02-13 15:57:26 +1300

    Update sentry

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,10 +1,10 @@
 import * as Sentry from '@sentry/browser';
-import { Integrations } from '@sentry/tracing';
+import { BrowserTracing } from '@sentry/tracing';
 
 try {
     Sentry.init({
         dsn: 'https://08cf17c769734a8a8a5b95cacde0c250@o491618.ingest.sentry.io/5557471',
-        integrations: [new Integrations.BrowserTracing()],
+        integrations: [new BrowserTracing()],
 
         tracesSampleRate: 1.0,
     });
